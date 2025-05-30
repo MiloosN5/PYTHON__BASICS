6e74276723ea921b5fdc63aa225a44b387d23dce
MiloosN5 <56539570+MiloosN5@users.noreply.github.com>
2025-05-30 18:51:35 +0200

    restoring lesson activation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # from lessons import data_types
-from lessons import numbers
-# from lessons import text
+# from lessons import numbers
+from lessons import text
 # from lessons import lists
 # from lessons import control_flow
